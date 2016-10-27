ead0aa9c885249cadfb892a9216723f1fdb49665
boiox_jia <boiox_jia@qq.com>
2016-10-27 22:51:44 +0800

    2016-10-27 完善微信公共类的通过网页拉去用户信息

--- extend/wechat/Wechat.php
+++ extend/wechat/Wechat.php
@@ -7,13 +7,19 @@ namespace extend\Wechat;
 
 class Wechat
 {
-    const API_BASE_URL = 'https://api.weixin.qq.com/cgi-bin';                   //基础接口URL
-    const AUTH_URL = '/token?grant_type=client_credential&';                    //获取access_token地址
+    const API_BASE_URL_PREFIX = 'https://api.weixin.qq.com';                    //api基础URL
+    const AUTH_URL = '/cgi-bin/token?grant_type=client_credential&';            //获取access_token地址
+
+    const OAUTH_PREFIX = 'https://open.weixin.qq.com';                          //oauth2认证基础URL
+    const OAUTH_AUTHORIZE_URL = '/connect/oauth2/authorize?';                   //oauth2授权地址
+
+    const OAUTH_TOKEN_URL = '/sns/oauth2/access_token?';                        //获取oauth2认证的access_token地址
 
 
     private $appid;                     //appid
     private $appsecret;                 //appsecret
     private $access_token;
+    private $user_token;                //通过code获取的access_token(和基础的access_token不是一回事)
     private $errCode;                   //错误码
     private $errMsg;                    //错误信息
     public function __construct($options)
@@ -35,7 +41,7 @@ class Wechat
             return $cache;
         }
 
-        $result = $this->http_get(self::API_BASE_URL . self::AUTH_URL . "appid=" . $this->appid . "&secret=" . $this->appsecret);
+        $result = $this->http_get(self::API_BASE_URL_PREFIX . self::AUTH_URL . "appid=" . $this->appid . "&secret=" . $this->appsecret);
         if($result){
             $json = json_decode($result);
             if($json){
@@ -152,4 +158,41 @@ class Wechat
         return false;
     }
 
+    /**
+     * 授权请求地址，获得code
+     * @param $callback                 回调地址
+     * @param string $state             自定义参数值
+     * @param string $scope             拉去授权方式 snsapi_base => 不弹出授权页面   snsapi_userinfo => 弹出授权页面
+     * @return string
+     */
+    public function getOauthUrl($callback,$state='',$scope='snsapi_base')
+    {
+        return self::OAUTH_PREFIX . self::OAUTH_AUTHORIZE_URL . 'appid=' . $this->appid . '&redirect_uri=' . $callback . '&response_type=code&scope=' . $scope . '&state=' . $state . '#wechat_redirect';
+    }
+
+    /**
+     * 获取Oauth的AccessToken
+     * @return bool
+     */
+    public function getOauthAccessToken()
+    {
+        $code = $_GET['code'] ? $_GET['code'] : '';
+        if(!$code) return false;
+
+        $result = http_get(self::API_BASE_URL_PREFIX . self::OAUTH_TOKEN_URL . 'appid=' . $this->appid . '&secret=' . $this->appsecret . '&code=' . $code . '&grant_type=authorization_code');
+
+        if($result){
+            $json = json_decode($result);
+            if(!$json || !empty($json('errcode'))){
+                $this->errCode = $json('errcode');
+                $this->errMsg = $json('errmsg');
+                return false;
+            }
+
+            $this->user_token = $json['access_token'];
+            return $json;
+        }
+        return false;
+    }
+    
 }
